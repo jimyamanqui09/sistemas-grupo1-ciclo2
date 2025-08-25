29dbdd95c7327c8b68638e3567279e5e7990b8f3
jimyamanqui09 <jimyamanqui@gmail.com>
2025-08-25 12:02:57 -0500

    Add files via upload

---
+++ sistemas-grupo1-ciclo2/ConceptosPoo/src/main/java/pe/edu/upeu/encapsulamiento/Estudiante.java
@@ -0,0 +1,28 @@
+package pe.edu.upeu.encapsulamiento;
+
+public class Estudiante {
+    private String codigo;
+    private String carrera;
+
+
+
+    public void trabajo(){
+        System.out.println("estudiar,investigar.......!"+
+                "en la carrera de "+carrera);
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+}
